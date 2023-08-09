fe1fd953a17dbe99141f066252d8b6a790e31408
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-09 17:47:01 +0200

    Make calculator prototype

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let display = "";
+
+let firstNum = "";
+let secondNum = "";
+let pickedOperator = "";
+
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -21,5 +27,74 @@ function operate(a, b, operator) {
         "*": multiply,
         "/": divide,
     }
-    return functions[operator](a, b);
+    return functions[operator](a, b).toString();
+}
+
+function isOperation(string) {
+    return ["*", "/", "-", "+"].includes(string);
+}
+
+function isSpecial(string) {
+    return ["C", "="].includes(string);
+}
+
+function updateDisplay() {
+    let displayDiv = document.querySelector(".display");
+    if (display.includes("NaN") || display.includes("Infinity")) {
+        displayDiv.innerText = "Oops";
+        display = "";
+        firstNum = "";
+        secondNum = "";
+        pickedOperator = "";
+    } else {
+        displayDiv.innerText = display;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let buttons = document.querySelectorAll(".buttons button");
+    buttons.forEach((button) => button.addEventListener("click", buttonClicked))
+})
+
+function buttonClicked(event) {
+    let buttonKey = this.dataset.key;
+    if (isSpecial(buttonKey)) {
+        if (buttonKey === "C") {
+            display = "";
+            firstNum = "";
+            secondNum = "";
+            pickedOperator = "";
+        } else if (buttonKey === "=") {
+            if (firstNum && secondNum && pickedOperator) {
+                display = operate(firstNum, secondNum, pickedOperator);
+                firstNum = display;
+                secondNum = "";
+                pickedOperator = "";
+            }
+        }
+    } else if (isOperation(buttonKey)) {
+        if (firstNum) {
+            if (pickedOperator && secondNum) {
+                display = operate(firstNum, secondNum, pickedOperator);
+                firstNum = display;
+                pickedOperator = buttonKey;
+                secondNum = "";
+            } else {
+                pickedOperator = buttonKey;
+                display = pickedOperator;
+            }
+            pickedOperator = buttonKey;
+        }
+    } else {
+        if (!(buttonKey === "." && display.includes("."))) {
+            if (!pickedOperator) {
+                firstNum += buttonKey;
+                display = firstNum;
+            } else {
+                secondNum += buttonKey;
+                display = secondNum;
+            }
+        }
+    }
+    updateDisplay();
 }
